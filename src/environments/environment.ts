593060e91a9993ab9334e23d96100752dd7acc1a
Mark Zipagang <mzipagang@gmail.com>
2018-06-08 18:11:43 -0700

    use port 8080 for server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:8090',
+  API_URL: 'http://localhost:8080',
   API_KEY: 'e3c6191f-f341-4f7c-8a41-55c290db7095',
   HOSTED_API_URL: 'https://test.mogl.com',
   TOKEN_TIMEOUT_MINUTES: 15.1
